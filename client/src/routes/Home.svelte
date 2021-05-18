48b914cb9cb0926d05404a9392951e4c429ede4f
eellpp <6888621+eellpp@users.noreply.github.com>
2021-05-18 10:44:02 +0800

    updated

--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -8,17 +8,31 @@
   }
 </script>
 
-<div class="content">
+<section class="section">
 <h1>Your number is {rand}!</h1>
 <button on:click={getRand}>Get a random number generated from flask server</button>
+</section>
 
-{#each books as book}
-<h3 class="title is-3">{book.NAME}</h3>
-<ol type="1">
-{#each book.SENTENCES as sentence}
+<section class="section">
+<div class="content">
 
-<ul>{sentence}</ul>
-{/each}
-</ol>
-{/each}
+
+
+    {#each books as book}
+        <h3 class="title is-3">{book.NAME}</h3>
+        <div class="field is-grouped">
+            <div class="control">
+                <input class="input" type="text" placeholder="Words to search">
+            </div>
+            <div class="control">
+                <button class="button is-link">Search</button>
+            </div>
+        </div>
+        <ol type="1">
+        {#each book.SENTENCES as sentence}
+            <ul>{sentence}</ul>
+        {/each}
+        </ol>
+    {/each}
 </div>
+</section>
