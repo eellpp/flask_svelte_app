5e41977d334d0f8afcadd1af1471d32eb97b085d
eellpp <6888621+eellpp@users.noreply.github.com>
2021-06-15 13:38:33 +0800

    updated

--- app/client/src/routes/tutorial/travelapp/TravelCategory.svelte
+++ app/client/src/routes/tutorial/travelapp/TravelCategory.svelte
@@ -1,50 +1,68 @@
 <script>
-    import TravelItem from "./TravelItem.svelte";
-    import { blurOnKey,getUID } from "./Utils";
-    export let category;
-    let item_name;
-    let is_packed = false;
-    $: items = category.items
+  import TravelItem from "./TravelItem.svelte";
+  import { blurOnKey, getUID } from "./Utils";
+  export let category;
+  let item_name;
+  let is_packed = false;
+  $: items = category.items;
 
-    function addItem(){
-        const is_duplicate =  category.items.some((item) => item.name.toLowerCase() === item_name.toLowerCase());
-        if (!is_duplicate) {
-            const item = {id:getUID(),name:item_name, is_packed:is_packed}
-            
-            const index = category.items.length
-            category.items[index] = item
-            category = category
-        }
-        console.log(category)
-        item_name = ""
+  function addItem() {
+    const is_duplicate = category.items.some(
+      (item) => item.name.toLowerCase() === item_name.toLowerCase()
+    );
+    if (!is_duplicate) {
+      const item = { id: getUID(), name: item_name, is_packed: is_packed };
+
+      const index = category.items.length;
+      category.items[index] = item;
+      category = category;
     }
-
+    console.log(category);
+    item_name = "";
+  }
+  function deleteCategory(){
+    console.log("Deleting category " + category.name)
+  }
 </script>
 
 <div class="box has-background-primary">
-<h4 class="title is-4">{category.name}</h4>
-<div class="field">
+  <div class="level">
+    <div class="level-item level-left">
+      <h4 class="title is-4">{category.name}</h4>
+    </div>
+    <span
+      class="icon is-small is-right"
+      on:click={deleteCategory}
+      style="pointer-events: all;cursor: pointer;">
+      <i class="fas fa-trash-alt" />
+    </span>
+  </div>
+  <div class="field">
     <div class="control">
-      <input class="input" type="text" bind:value={item_name}>
+      <input class="input" type="text" bind:value={item_name} />
     </div>
     <div class="control">
-        <label class="radio">
-          <input type="radio" name="packed" on:select={() => is_packed = true} />
-          Packed
-        </label>
-        <label class="radio">
-          <input type="radio" name="packed" on:select={() => is_packed = false} checked>
-          NotPacked
-        </label>
-      </div>
+      <label class="radio">
+        <input
+          type="radio"
+          name="packed"
+          on:select={() => (is_packed = true)} />
+        Packed
+      </label>
+      <label class="radio">
+        <input
+          type="radio"
+          name="packed"
+          on:select={() => (is_packed = false)}
+          checked />
+        NotPacked
+      </label>
+    </div>
     <div class="control is-grouped ">
-      <a class="button is-info" on:click={addItem}>
-        AddItem
-      </a>
+      <a class="button is-info" on:click={addItem}> AddItem </a>
     </div>
   </div>
-{#each items as item}
-    <TravelItem bind:item={item} />
-{/each}
+  {#each items as item}
+    <TravelItem bind:item />
+  {/each}
 </div>
-
--- app/client/src/routes/tutorial/travelapp/Travelitem.svelte
+++ app/client/src/routes/tutorial/travelapp/Travelitem.svelte
@@ -1,20 +1,43 @@
 <script>
-import {blurOnKey} from "./Utils"
+    import { blurOnKey } from "./Utils";
+    let yes = false;
 
-export let item;
-let editing=false;
-// item has name,category,ispacked fields
+    export let item;
+    let editing = false;
+    // item has name,category,ispacked fields
 
+    function deleteItem() {
+        console.log("Deleting the item " + item.name);
+    }
 </script>
 
 <div class="control">
     <label class="checkbox">
-        <input type="checkbox">
         
-    {#if editing === false}
-    <span class="span" on:click={() => editing = true} >{item.name}</span>
-    {:else}
-    <input class="input" on:blur={() => editing = false} on:keypress={blurOnKey} bind:value={item.name}/>
-    {/if}
-</label>
+        <input type="checkbox" bind:checked={yes}/>
+
+        {#if editing === false}
+            {#if yes}
+            <span
+                class="span"
+                on:click={() => (editing = true)} style="text-decoration: line-through">{item.name}</span>
+            {:else}
+            <span
+                class="span"
+                on:click={() => (editing = true)}>{item.name}</span>
+            {/if}
+        {:else}
+            <input
+                class="input"
+                on:blur={() => (editing = false)}
+                on:keypress={blurOnKey}
+                bind:value={item.name} />
+        {/if}
+    </label>
+    <span
+        class="icon is-small is-right"
+        on:click={deleteItem}
+        style="pointer-events: all;cursor: pointer;">
+        <i class="fas fa-trash-alt" />
+    </span>
 </div>
